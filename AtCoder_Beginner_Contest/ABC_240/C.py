f33998891af48fbdaf0e39ac46df7b9e8d51b302
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-02-23 17:32:39 +0900

    ABC_240_C_D

--- AtCoder_Beginner_Contest/ABC_240/C.py
+++ AtCoder_Beginner_Contest/ABC_240/C.py
@@ -38,47 +38,47 @@
 
 # 0,1,2,3,4,5
 
-import sys
-sys.setrecursionlimit(10**6)
+# import sys
+# sys.setrecursionlimit(10**6)
 
-N, X = map(int, input().split())
-jump_num = [list(map(int, input().split())) for _ in range(N)]
-max_sum = 0
-min_sum = 0
+# N, X = map(int, input().split())
+# jump_num = [list(map(int, input().split())) for _ in range(N)]
+# max_sum = 0
+# min_sum = 0
 
-for i, j in jump_num:
-    if i > j:
-        max_sum += i
-        min_sum += j
-    else:
-        max_sum += j
-        min_sum += i
+# for i, j in jump_num:
+#     if i > j:
+#         max_sum += i
+#         min_sum += j
+#     else:
+#         max_sum += j
+#         min_sum += i
 
-if not (min_sum <= X <= max_sum):
-    print('No')
-    exit()
-elif (min_sum == X) or (max_sum == X):
-    print('Yes')
-    exit()
+# if not (min_sum <= X <= max_sum):
+#     print('No')
+#     exit()
+# elif (min_sum == X) or (max_sum == X):
+#     print('Yes')
+#     exit()
     
 
 
-def dfs(index: int, sum: int):
+# def dfs(index: int, sum: int):
     
-    if (index == N) and (sum == X):
-        print('Yes')
-        exit()
-    elif (index == N):
-        return
+#     if (index == N) and (sum == X):
+#         print('Yes')
+#         exit()
+#     elif (index == N):
+#         return
     
-    if (sum > X) or (sum + 100*(N-index) < X):
-        return
+#     if (sum > X) or (sum + 100*(N-index) < X):
+#         return
     
-    dfs(index+1, sum+jump_num[index][0])
-    dfs(index+1, sum+jump_num[index][1])
+#     dfs(index+1, sum+jump_num[index][0])
+#     dfs(index+1, sum+jump_num[index][1])
 
-dfs(0, 0)
-print('No')
+# dfs(0, 0)
+# print('No')
 
 
  
@@ -126,4 +126,37 @@ print('No')
 #             que.append([index+1, sum + jump_num[index][j]])
 
 # print('No')
+
+
+
+
+
+# DPで解きます。
+# DP[i][j]: i回目のジャンプが終わった後に j にいることができるのか
+# DP[i][j] = DP[i-1][j-jump(i)] or DP[i-1][j-jump(i)] -> True
+
+N, X = map(int, input().split())
+jump_num = [0] + [list(map(int, input().split())) for _ in range(N)]
+DP = [[False] * (X+1) for _ in range(N+1)]
+DP[0][0] = True
+
+for i in range(1, N+1):
+    jump_a = jump_num[i][0]
+    jump_b = jump_num[i][1]
+    for j in range(X+1):
+        if DP[i-1][j]:
+            if j+jump_a <= X:
+                DP[i][j+jump_a] = True
+            if j+jump_b <= X:
+                DP[i][j+jump_b] = True
+
+if DP[N][X]:
+    print('Yes')
+else:
+    print('No')
+
+
+
+
+
 
--- AtCoder_Beginner_Contest/ABC_240/D.py
+++ AtCoder_Beginner_Contest/ABC_240/D.py
@@ -1,3 +1,32 @@
+# [number, count] の形で、筒の中に積み重ねていく。
+# number = count になったら、このリストは消える。
+# 圧縮操作みたいなもの
+# stack: スタックとは、最も基本的なデータ構造の一つで、要素が入ってきた順に一列に並べ、後に入れた要素から順に取り出すという規則で出し入れを行うもの。
+
+N = int(input())
+ball_number = list(map(int, input().split()))
+stack = []
+ball_count = 0
+
+for i in range(N):
+    if (len(stack) == 0) or (stack[-1][0] != ball_number[i]):
+        stack.append([ball_number[i], 1])
+        ball_count += 1
+        print(ball_count)
+    elif stack[-1][0] == ball_number[i]:
+        stack[-1][1] += 1
+        ball_count += 1
+        if stack[-1][0] == stack[-1][1]:
+            ball_count -= stack[-1][1]
+            stack.pop()
+            print(ball_count)
+        else:
+            print(ball_count)
+
+
+
+        
+
 
 
 
