975275e6969d3a3e077324a51b3e86efda770bae
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-03-22 11:40:35 +0900

    binary_serch

--- binary_serch.py
+++ binary_serch.py
@@ -17,28 +17,58 @@
 
 # 入力のサイズが10**8以上だったら、O(log(N))の方法で行ける。
 
-nums = list(map(int, input().split()))
-left = 0
-right = len(nums) - 1
+# nums = list(map(int, input().split()))
+# left = 0
+# right = len(nums) - 1
 
-if nums[0] < nums[-1]:
-    print(nums[0])
-    exit()
+# if nums[0] < nums[-1]:
+#     print(nums[0])
+#     exit()
 
-while right - left > 1:
-    mid = (right + left) // 2
-    if nums[left] < nums[mid]:
-        left = mid
-    else:
-        right = mid
+# while right - left > 1:
+#     mid = (right + left) // 2
+#     if nums[left] < nums[mid]:
+#         left = mid
+#     else:
+#         right = mid
 
-print(nums[right])
-
-
+# print(nums[right])
 
 
 
 
+# 練習するぜ
+# 昇順をスライドさせてできた数列の中で最小値を見つける。
+# 4 5 1 2 3
+
+import sys
+from typing import List
+
+def main(lines: List):
+    numbers = list(map(int, lines[0].split()))
+    left = 0
+    right = len(numbers) - 1
+
+    if numbers[left] < numbers[right]:
+        print(numbers[left])
+        exit()
+    
+    while right - left > 1:
+        mid = (left + right) // 2
+        if numbers[left] < numbers[mid]:
+            left = mid
+        else:
+            right = mid
+    
+    print(numbers[right])
+
+
+
+if __name__ == '__main__':
+    lines = []
+    for l in sys.stdin:
+        lines.append(l.rstrip('\r\n'))
+    main(lines)
 
 
 
