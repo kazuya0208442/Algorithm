794eef55b7493923e723a91c9f85ae64d0e112bd
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-03-27 10:27:17 +0900

    ABC_233_C.py

--- DFS-Depth_First_Search/ABC_233_C.py
+++ DFS-Depth_First_Search/ABC_233_C.py
@@ -1,9 +1,35 @@
 import sys
 from typing import List
-from collections import deque
+sys.setrecursionlimit(10**6)
 
 def main(lines: List):
+    N, X = map(int, lines[0].split())
+    ball_numbers = []
+    global ans
+    ans = 0
+
+    for s in lines[1:]:
+        L, *numbers = map(int, s.split())
+        ball_numbers.append(numbers)
     
+    def dfs(index: int, product: int):
+
+        if (index == N-1) and (product == X):
+            global ans
+            ans += 1
+            return
+        elif index == N-1:
+            return
+
+        for v in ball_numbers[index+1]:
+            if product * v > X:
+                continue
+            dfs(index+1, product*v)
+    
+    for v in ball_numbers[0]:
+        dfs(0, v)
+    
+    print(ans)
 
 
 if __name__ == '__main__':
